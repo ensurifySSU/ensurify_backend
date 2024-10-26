dfa4184b4d19f34b8c36250ace529917d99cc638
yueunfive <122276414+yueunfive@users.noreply.github.com>
2024-10-26 22:00:12 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/com/example/inssurify/InssurifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.inssurify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InssurifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InssurifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inssurify/InssurifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inssurify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InssurifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
