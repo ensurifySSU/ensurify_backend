4e58ca074732ce3a84aa0259d7992aa0f18954c2
yueunfive <122276414+yueunfive@users.noreply.github.com>
2024-11-13 11:11:07 +0900

    refactor: @SpringBootTest 어노테이션을 제거

--- src/test/java/com/example/inssurify/InssurifyApplicationTests.java
+++ src/test/java/com/example/inssurify/InssurifyApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.inssurify;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class InssurifyApplicationTests {
 
     @Test
