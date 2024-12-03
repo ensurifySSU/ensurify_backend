a8b22e00df7364fbcb834ab100faa129bcc9e81b
yueunfive <122276414+yueunfive@users.noreply.github.com>
2024-12-03 12:54:38 +0900

    refactor: CORS allowedOriginPatterns 추가

--- src/main/java/com/example/ensurify/common/config/CorsConfig.java
+++ src/main/java/com/example/ensurify/common/config/CorsConfig.java
@@ -19,6 +19,8 @@ public class CorsConfig {
         ArrayList<String> allowedOriginPatterns = new ArrayList<>();
         allowedOriginPatterns.add("http://localhost:3000");
         allowedOriginPatterns.add("http://127.0.0.1:3000");
+        allowedOriginPatterns.add("http://localhost:8080");
+        allowedOriginPatterns.add("https://ensurify.store");
         configuration.setAllowedOrigins(allowedOriginPatterns);
 
         //허용하는 HTTP METHOD 지정
